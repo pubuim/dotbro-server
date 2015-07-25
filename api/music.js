a321bfa42b7d4d243a3d3f96c80ba4738a4be442
trigged <trigged@gmail.com>
2015-07-25 18:17:18 +0800

    零时提交

--- api/music.js
+++ api/music.js
@@ -2,19 +2,29 @@
  * Created by trigged on 7/25/15.
  */
 const WYMusic = require("../services/wymusic")
+  , Song = require("../models/song")
+  , playlist = require("../models/playlist")
+  , _ = require("lodash")
 
 
 var music = {
   list: function *() {
-    this.body =  "list"
+    this.body = playlist.values()
   },
 
-  add :function *(){
-    this.body = yield WYMusic.analysis(this.request.body.url)
+  add: function *() {
+    var result  = yield WYMusic.analysis(this.request.body.url)
+    if(is_string(result)){
+      this.body = {
+        code : 0,
+        error_desc : result
+      }
+    }
+    var song =new Song(this.body)
 
   },
   remove: function *(wyID) {
-    this.body =  `remove ${wyID}`
+    this.body = `remove ${wyID}`
   }
 };
 module.exports = music
