47e25d86e3d5712df7b32fbfeddd22e2d70ad7fd
gordomium <x@gordomium.me>
2015-07-25 16:49:45 +0800

    add gitignores

--- api/music.js
+++ api/music.js
@@ -1,17 +1,17 @@
 /**
  * Created by trigged on 7/25/15.
  */
-const WYMusic = require("../services/wymusic")
+const WYMusic = require('../services/wymusic')
 
 
 var music = {
   list: function *() {
-    this.body =  "list"
+    this.body =  'list'
   },
 
   add :function *(){
     this.body = yield WYMusic.analysis(this.request.body.url)
-
+    
   },
   remove: function *(wyID) {
     this.body =  `remove ${wyID}`
